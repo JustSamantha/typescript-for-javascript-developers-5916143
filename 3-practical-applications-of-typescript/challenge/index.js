12217ea203169c361abca2d53bf496169f4317f9
Samantha Castillo <samanthacas@pm.me>
2025-02-06 08:51:36 -0800

    - Adds configuration files - Migrates javascript project to typescript

--- 3-practical-applications-of-typescript/challenge/index.js
+++ 3-practical-applications-of-typescript/challenge/index.js
@@ -1,7 +1,7 @@
-const { addUser, getUser } = require('./user');
-
-addUser({ id: 1, name: 'Jeema', email: 'jeema@example.com' });
-addUser({ id: 2, name: 'Ali', email: 'ali@example.com' });
-
-console.log(getUser(1));
-console.log(getUser(3));  // Should return 'User not found'
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user_1 = require("./user");
+(0, user_1.addUser)({ id: 1, name: 'Jeema', email: 'jeema@example.com' });
+(0, user_1.addUser)({ id: 2, name: 'Ali', email: 'ali@example.com' });
+console.log((0, user_1.getUser)(1));
+console.log((0, user_1.getUser)(3)); // Should return 'User not found'
--- 3-practical-applications-of-typescript/challenge/user.js
+++ 3-practical-applications-of-typescript/challenge/user.js
@@ -1,16 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addUser = addUser;
+exports.getUser = getUser;
 const users = [];
-
 function addUser(user) {
-  users.push(user);
+    users.push(user);
 }
-
 function getUser(id) {
-  const user = users.find(u => u.id === id);
-  if (user) {
-    return user;
-  } else {
-    return 'User not found';
-  }
+    const user = users.find(u => u.id === id);
+    if (user) {
+        return user;
+    }
+    else {
+        return 'User not found';
+    }
 }
-
-module.exports = { addUser, getUser };
